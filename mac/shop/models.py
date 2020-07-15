94ca33c770bb258013f9d009137cb1b413a304a9
Deepak07866 <68264196+Deepak07866@users.noreply.github.com>
2020-07-15 09:12:27 +0530

    commit

---
+++ mac/shop/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    Product_id=models.AutoField
+    Product_name=models.CharField(max_length=50)
+    decs=models.CharField(max_length=300)
+    pub_date=models.DateField()
+    category=models.CharField(max_length=50, default="")
+    subcategory=models.CharField(max_length=50, default="")
+    price=models.IntegerField(default=0)
+    image=models.ImageField(upload_to="shop/images", default="")
+
+    def __str__(self):
+        return self.Product_name
+
+class Contact(models.Model):
+    msg_id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50)
+    decs=models.CharField(max_length=300, default="")
+    email=models.CharField(max_length=50, default="")
+    phone=models.CharField(max_length=50, default="")
+
+
+    def __str__(self):
+        return self.name
